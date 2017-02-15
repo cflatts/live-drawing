4d1b785d8c9eb12a60fd21e399d787536fdf7c90
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-15 16:33:06 -0600

    express server working, still not displaying everything because formatted for lite-server

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var app = express()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
 
-PORT = 3000
+server.listen(3000)
 
-app.listen(PORT)
+app.use(express.static('src'))
 
-app.engine('html', './src/index.html')
-
-console.log('listening on port:' + PORT)
+app.get('/', function(req, res) {
+    res.sendFile( __dirname + '/src/app/index.html')
+})
