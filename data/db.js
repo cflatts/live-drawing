2742357eb8fb0bba81062fc9702dcb5ce48194e4
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-15 14:30:14 -0600

    ran npm init and updated package.json

--- data/db.js
+++ data/db.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/live-drawing')
 
+
 var db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'connection error:'))
