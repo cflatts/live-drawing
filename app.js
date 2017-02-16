755683fef0f36876f39e20cd9105154c406c8f17
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-16 14:27:23 -0600

    server running, requirement errors

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'src')));
 app.use('/', index);
 app.use('/users', users);
 
-catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
