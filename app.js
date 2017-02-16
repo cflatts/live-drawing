46bfb7cb4fb07a99b7e620a15fba6a0979ae21df
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-16 14:36:20 -0600

    back to my original app that im building because the errors are the same

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+
+var PORT = 3000
+server.listen(3000)
+
+app.listen(PORT)
+app.use(express.static(path.join(__dirname, 'src')))
+
+app.engine('html', './src/index.html')
+
+console.log('listening on port:' + PORT)
+
+app.get('/', function(req, res) {
+   res.sendFile( __dirname + '/src/app/index.html')
+})
