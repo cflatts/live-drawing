a1c106df06cdb0fe6b9b3ad01e1a7e6d0c40f1fd
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-16 16:13:04 -0600

    made some changes

--- app.js
+++ app.js
@@ -9,20 +9,22 @@ var PORT = 3000
 server.listen(PORT)
 
 // app.listen(PORT)
-app.use(express.static(path.join(__dirname, 'src')))
+app.use(express.static('src'))
 
-app.set('html', renderFile)
-app.set('view engine', 'ejs')
+// app.set('html', renderFile)
+// app.set('view engine', 'ejs')
 
 console.log('listening on port:' + PORT)
 
-app.get('/', function(req, res) {
-   res.sendFile( __dirname + '/src/index.html')
-})
+// app.get('/home', function(req, res) {
+//     console.log(req)
+//     console.log(res)
+//    res.sendFile( __dirname + '/src/index.html')
+// })
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' })
-  socket.on('my other event', function (data) {
-    console.log(data)
-  });
-})
+// io.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' })
+//   socket.on('my other event', function (data) {
+//     console.log(data)
+//   });
+// })
--- data/db.js
+++ data/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/live-drawing')
+mongoose.connect('mongodb://localhost/test')
 
 
 var db = mongoose.connection
