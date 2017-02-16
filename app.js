38cbc7e3d410507d4fce1de1e6c1ad4128cf9fad
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-16 14:21:31 -0600

    src becomes main client side code

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var renderFile = require('ejs').renderFile
 require('dotenv').config()
 
 var index = require('./routes/index');
@@ -11,8 +12,8 @@ var users = require('./routes/users');
 
 var app = express();
 
-// // app.set('views', path.join(__dirname, 'client'));
-// app.set('view engine', 'jade');
+app.set('html', renderFile);
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,12 +21,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
