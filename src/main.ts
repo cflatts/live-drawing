e731502f722417c13e4790e02ee00997c0fcbb3f
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-16 13:36:52 -0600

    modified express server and returned main.ts back to original state

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-var platformBrowserDynamic = require('@angular')
-var AppModule = require('./app/app.module')
-
-// import { AppModule } from './app/app.module';
+import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
