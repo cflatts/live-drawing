8c602bbcf9560fc0c1db458a65caa800a1562260
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-17 07:32:34 -0600

    turned app into angular-cli app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { NgModule }      from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent }  from './app.component'
-import {HomeComponent} from './home/home.component'
-import {DrawingComponent} from './drawing/drawing.component'
-import {LoginComponent} from './login/login.component'
-
-import {AppRoutingModule} from './app-routing.module'
-
-import {DrawingService} from './drawing/drawing.service'
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, AppRoutingModule ],
-  declarations: [ AppComponent, HomeComponent, DrawingComponent, LoginComponent ],
-  providers: [DrawingService],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
