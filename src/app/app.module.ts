e72eda33e06763258f12251a3a673b43a5d4ee1e
Caitlin Flattery <caitlinflattery@gmail.com>
2017-02-17 07:43:37 -0600

    re-wrote app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component'
+import {HomeComponent} from './home/home.component'
+import {DrawingComponent} from './drawing/drawing.component'
+import {LoginComponent} from './login/login.component'
+
+import {AppRoutingModule} from './app-routing.module'
+
+import {DrawingService} from './drawing/drawing.service'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, AppRoutingModule, FormsModule, HttpModule ],
+  declarations: [ AppComponent, HomeComponent, DrawingComponent, LoginComponent ],
+  providers: [DrawingService],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
